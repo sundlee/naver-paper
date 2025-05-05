2dc6f705eca3cde0083e97c39bfedb8d0b8a504a
sundlee <sundlee@gmail.com>
2025-05-05 17:53:40 +0900

    Update run_new.py

--- run_new.py
+++ run_new.py
@@ -238,7 +238,7 @@ if __name__ == "__main__":
         if(pw is None and pw is None and cd_env is None):
             print('not setting USERNAME / PASSWORD')
             exit()
-        if cd_env is None and cd_env == "":
+        if cd_env is None:
             cd_obj = [{"id": id, "pw": pw}]
         else:
             cd_obj = json.loads(cd_env)
